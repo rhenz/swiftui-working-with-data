2f97f82a2ccdc59a5c19ac42f8dd8288d383904f
rhenz <salvador.rhenz@gmail.com>
2023-02-08 11:40:17 +0800

    Update variable names

--- SwiftUI_WorkingWithData/Common/HeaderView.swift
+++ SwiftUI_WorkingWithData/Common/HeaderView.swift
@@ -10,16 +10,16 @@ import SwiftUI
 struct HeaderView: View {
    
    var title: String
+   var subtitle: String
    var desc: String
-   var content: String
    var backgroundColor = Color.blue
    
    var body: some View {
       VStack(spacing: 15) {
          Text(title)
-         Text(desc)
+         Text(subtitle)
             .foregroundColor(.gray)
-         Text(content)
+         Text(desc)
             .padding()
             .frame(maxWidth: .infinity)
             .background(backgroundColor)
@@ -32,6 +32,6 @@ struct HeaderView: View {
 
 struct HeaderView_Previews: PreviewProvider {
    static var previews: some View {
-      HeaderView(title: "Title", desc: "Description", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque non tristique sapien. Aenean nulla eros, laoreet nec dictum non, cursus sed magna. Duis tincidunt justo tellus. In pulvinar pretium ante, eu euismod mi mattis ut.")
+      HeaderView(title: "Title", subtitle: "Description", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque non tristique sapien. Aenean nulla eros, laoreet nec dictum non, cursus sed magna. Duis tincidunt justo tellus. In pulvinar pretium ante, eu euismod mi mattis ut.")
    }
 }
---
+++ SwiftUI_WorkingWithData/Struct Properties/StructVariables.swift
@@ -0,0 +1,24 @@
+//
+//  StructVariables.swift
+//  SwiftUI_WorkingWithData
+//
+//  Created by John Salvador on 2/8/23.
+//
+
+import SwiftUI
+
+struct StructVariables: View {
+   var name = "Lucas"
+   
+   var body: some View {
+      VStack(spacing: 20) {
+         
+      }
+   }
+}
+
+struct StructVariables_Previews: PreviewProvider {
+   static var previews: some View {
+      StructVariables()
+   }
+}
